d89e123b463d16054bb76183e7a7820e470176fe
Patrick Konieczeny <nicolas.bazely@gmail.com>
2018-05-28 14:11:15 +0200

    Initial Commit

---
+++ src/main/java/com/Edc2/Epsi/BranswickBazelyMahiette/BranswickBazelyMahietteApplication.java
@@ -0,0 +1,12 @@
+package com.Edc2.Epsi.BranswickBazelyMahiette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranswickBazelyMahietteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranswickBazelyMahietteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Edc2/Epsi/BranswickBazelyMahiette/BranswickBazelyMahietteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Edc2.Epsi.BranswickBazelyMahiette;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BranswickBazelyMahietteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
